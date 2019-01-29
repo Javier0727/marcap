593981c330daec5fa3de14c0868f2b6ea14b606d
Javier Jael <javier.castillo@studentpartner.com>
2019-01-29 05:28:21 -0600

    Final Responsive

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
     onload = function () {
-             document.getElementById('bchat').scrollTop = document.getElementById('bchat').scrollHeight;
+        document.getElementById('bchat').scrollTop = document.getElementById('bchat').scrollHeight;
+    }
+    function desplazar() {
+        var contador = 1;
+        selector = document.getElementById("despla");
+        if (contador == 1) {
+            selector.style.left = "0%";
+            contador ++;
+        }else{
+            selector.style.left = "-100%";
+            contador = 1;
+        }
     }
